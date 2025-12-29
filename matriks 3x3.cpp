d4a58dd8d2eeb21af25b3c4766e5e38bfc71e2a2
dinataa <ragildinata1123@gmail.com>
2025-12-29 15:49:29 +0700

    Update matriks 3x3.cpp

--- matriks 3x3.cpp
+++ matriks 3x3.cpp
@@ -5,9 +5,6 @@ int main() {
     int A[3][3], B[3][3], hasil[3][3];
     int pilihan;
     
-    cout << "=== PROGRAM OPERASI MATRIKS 3x3 ===" << endl;
-    cout << endl;
-    
     cout << "Masukkan elemen Matriks A (3x3):" << endl;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
